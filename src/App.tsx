d2141dfcb8b2fa1cee2cec61295dc7b96fb1897a
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-04-13 02:20:01 +0430

    Added basic functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import FilePicker from "./components/FilePicker";
 
 function App() {
--- src/components/FilePicker/index.tsx
+++ src/components/FilePicker/index.tsx
@@ -1,24 +1,110 @@
+import { useState, useRef } from "react";
+import axios from "axios";
+import { CircularProgressbar } from "react-circular-progressbar";
+import { ReactComponent as Upload } from "../../assets/svg/upload.svg";
 import "./style.scss";
 
-export default function FilePicker() {
+interface FilePickerProps {
+  [key: string]: any;
+  fileSizeLimit?: number;
+}
+
+export default function FilePicker({ fileSizeLimit }: FilePickerProps) {
+  const labelRef = useRef<HTMLLabelElement>(null);
+  const [isAccepted, setIsAccepted] = useState(false);
+  const [isUploading, setIsUploading] = useState(false);
+  const [uploadPercent, setUploadPercent] = useState(0);
+  const [error, setError] = useState("");
+
+  const onSubmit = (e: any) => {
+    e.preventDefault();
+
+    // reset upload progress
+    setUploadPercent(0);
+  };
+
+  const onDragCheck = (e: any) => {
+    e.stopPropagation();
+    e.preventDefault();
+
+    // reset upload progress
+    setUploadPercent(0);
+
+    if (labelRef.current) {
+      labelRef.current.className = e.type === "dragover" ? "dragged" : "file-upload";
+    }
+  };
+
+  const parseFile = (file: any) => {
+    setError(encodeURI(file.name));
+    var imageName = file.name;
+    var isBad = /\.(?=exe)/gi.test(imageName);
+    setIsAccepted(!isBad);
+  };
+
+  const uploadFile = (file: any) => {
+    fileSizeLimit = fileSizeLimit || 1;
+    if (file.size <= fileSizeLimit * 1024 * 1024) {
+      setIsUploading(true);
+
+      const data = new FormData();
+      data.append("file", file);
+      data.append("upload_preset", "ml_default");
+      axios.post("https://api.cloudinary.com/v1_1/dhvgwaugh/image/upload", data, {
+        onUploadProgress: (e) => {
+          const percent = Math.floor((e.loaded / file.size) * 100);
+          setUploadPercent(percent > 100 ? 100 : percent);
+        },
+      });
+    } else {
+      setIsAccepted(false);
+      setIsUploading(false);
+      setError(`حجم فایل موردنظر باید کمتر از ${fileSizeLimit} مگابایت باشد.`);
+    }
+  };
+
+  const fileSelectHandler = (e: any) => {
+    // Fetch FileList object
+    var files = e.target.files || e.dataTransfer.files;
+    // Cancel event and hover styling
+    onDragCheck(e);
+    // Process all File objects
+    for (var i = 0, f; (f = files[i]); i++) {
+      parseFile(f);
+      uploadFile(f);
+    }
+  };
+
   return (
-    <form id="file-upload-form" className="uploader">
-      <input id="file-upload" type="file" name="fileUpload" />
-      <label htmlFor="file-upload" id="file-drag">
-        <img id="file-image" src="#" alt="Preview" className="hidden" />
-        <div id="start">
-          <div>یک فایل انتخاب کنید یا به اینجا بکشید.</div>
-          <button id="file-upload-btn" className="btn btn-primary">
-            انتخاب فایل
-          </button>
-        </div>
-        <div id="response" className="hidden">
-          <div id="messages"></div>
-          <progress className="progress" id="file-progress" value="0">
-            <span>0</span>%
-          </progress>
-        </div>
-      </label>
-    </form>
+    <div className="uploader-wrapper">
+      <form className="uploader" onSubmit={onSubmit}>
+        <input type="file" id="file-upload" name="fileUpload" onChange={fileSelectHandler} multiple />
+        <label
+          ref={labelRef}
+          htmlFor="file-upload"
+          className="upload-label"
+          onDragLeave={onDragCheck}
+          onDragOver={onDragCheck}
+          onDrop={fileSelectHandler}
+        >
+          <div hidden={isUploading} className="upload-start">
+            <Upload className="upload-icon" />
+            <div>یک فایل انتخاب کنید یا به اینجا بکشید</div>
+            <span className="btn select-button">انتخاب فایل</span>
+          </div>
+          <div className="upload-info" hidden={!isUploading}>
+            {isUploading && (
+              <CircularProgressbar
+                className="progress"
+                value={uploadPercent}
+                text={`${uploadPercent}%`}
+                strokeWidth={3}
+              />
+            )}
+          </div>
+          {error && <div className="upload-message">{error}</div>}
+        </label>
+      </form>
+    </div>
   );
 }
