6b28daaddc016d49a4cc72b1fc4322a3f725f0e3
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-04-13 03:12:02 +0430

    Improved logging errors and styles

--- src/components/FilePicker/index.tsx
+++ src/components/FilePicker/index.tsx
@@ -11,10 +11,11 @@ interface FilePickerProps {
 
 export default function FilePicker({ fileSizeLimit }: FilePickerProps) {
   const labelRef = useRef<HTMLLabelElement>(null);
-  const [isAccepted, setIsAccepted] = useState(false);
   const [isUploading, setIsUploading] = useState(false);
+  const [isAccepted, setIsAccepted] = useState(false);
   const [uploadPercent, setUploadPercent] = useState(0);
-  const [error, setError] = useState("");
+  const [message, setMessage] = useState("");
+  const [uploadedList, setUploadedList] = useState<string[]>([]);
 
   const onSubmit = (e: any) => {
     e.preventDefault();
@@ -31,47 +32,58 @@ export default function FilePicker({ fileSizeLimit }: FilePickerProps) {
     setUploadPercent(0);
 
     if (labelRef.current) {
-      labelRef.current.className = e.type === "dragover" ? "dragged" : "file-upload";
+      labelRef.current.className = e.type === "dragover" ? "dragged" : "upload-label";
     }
   };
 
   const parseFile = (file: any) => {
-    setError(encodeURI(file.name));
-    var imageName = file.name;
-    var isBad = /\.(?=exe)/gi.test(imageName);
-    setIsAccepted(!isBad);
+    const isBad = /\.(?=exe)/gi.test(file.name);
+    if (isBad) {
+      setIsAccepted(false);
+      setIsUploading(false);
+      setMessage("نوع فایل موردنظر پشتیبانی نمی‌شود.");
+    } else {
+      setIsAccepted(true);
+      setMessage(encodeURI(file.name));
+      uploadFile(file);
+    }
   };
 
-  const uploadFile = (file: any) => {
-    fileSizeLimit = fileSizeLimit || 1;
-    if (file.size <= fileSizeLimit * 1024 * 1024) {
-      setIsUploading(true);
+  const uploadFile = async (file: any) => {
+    setIsUploading(true);
 
-      const data = new FormData();
-      data.append("file", file);
-      data.append("upload_preset", "ml_default");
-      axios.post("https://api.cloudinary.com/v1_1/dhvgwaugh/image/upload", data, {
-        onUploadProgress: (e) => {
-          const percent = Math.floor((e.loaded / file.size) * 100);
-          setUploadPercent(percent > 100 ? 100 : percent);
-        },
-      });
+    fileSizeLimit = fileSizeLimit || 1;
+    if (isAccepted || file.size <= fileSizeLimit * 1024 * 1024) {
+      const uploadData = new FormData();
+      uploadData.append("file", file);
+      uploadData.append("upload_preset", "ml_default");
+
+      try {
+        const { data }: any = await axios.post("https://api.cloudinary.com/v1_1/dhvgwaugh/image/upload", uploadData, {
+          onUploadProgress: (e) => {
+            const percent = Math.floor((e.loaded / file.size) * 100);
+            setUploadPercent(percent > 100 ? 100 : percent);
+          },
+        });
+        setUploadedList((old) => [...old, data.secure_url]);
+      } catch (e) {
+        console.error(e);
+      }
     } else {
       setIsAccepted(false);
       setIsUploading(false);
-      setError(`حجم فایل موردنظر باید کمتر از ${fileSizeLimit} مگابایت باشد.`);
+      setMessage(`حجم فایل موردنظر باید کمتر از ${fileSizeLimit} مگابایت باشد.`);
     }
   };
 
   const fileSelectHandler = (e: any) => {
     // Fetch FileList object
-    var files = e.target.files || e.dataTransfer.files;
+    const files = e.target.files || e.dataTransfer.files;
     // Cancel event and hover styling
     onDragCheck(e);
     // Process all File objects
-    for (var i = 0, f; (f = files[i]); i++) {
+    for (let i = 0, f; (f = files[i]); i++) {
       parseFile(f);
-      uploadFile(f);
     }
   };
 
@@ -87,24 +99,33 @@ export default function FilePicker({ fileSizeLimit }: FilePickerProps) {
           onDragOver={onDragCheck}
           onDrop={fileSelectHandler}
         >
-          <div hidden={isUploading} className="upload-start">
-            <Upload className="upload-icon" />
-            <div>یک فایل انتخاب کنید یا به اینجا بکشید</div>
-            <span className="btn select-button">انتخاب فایل</span>
-          </div>
-          <div className="upload-info" hidden={!isUploading}>
-            {isUploading && (
+          {!isUploading ? (
+            <div className="upload-start">
+              <Upload className="upload-icon" />
+              <div>یک فایل انتخاب کنید یا به اینجا بکشید</div>
+              <span className="btn select-button">انتخاب فایل</span>
+            </div>
+          ) : (
+            <div className="upload-info">
               <CircularProgressbar
                 className="progress"
                 value={uploadPercent}
                 text={`${uploadPercent}%`}
                 strokeWidth={3}
               />
-            )}
-          </div>
-          {error && <div className="upload-message">{error}</div>}
+            </div>
+          )}
+          {message && <div className="upload-message">{message}</div>}
         </label>
       </form>
+      <div className="uploaded-list">
+        {uploadedList?.map((item) => (
+          <div>
+            <input type="text" readOnly value={item} />
+            <button>Copy</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
